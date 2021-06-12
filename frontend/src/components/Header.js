7e6a32909eb420d162c3036ae14feb0c5d42f513
mosesvk <kaumatule93@gmail.com>
2021-06-12 00:47:26 -0600

    update

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Navbar, Nav, Container, Row, NavDropdown } from 'react-bootstrap'
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
 
 
 const Header = () => {
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import products from '../products'
 import Product from '../components/Product'
-import { Row, Col, Image } from 'react-bootstrap'
+import { Row, Col } from 'react-bootstrap'
 
 const HomeScreen = () => {
   return (
