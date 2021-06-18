53d61dcdd5aace9ab16cf9e6558c664dfeaa0ce1
mosesvk <kaumatule93@gmail.com>
2021-06-18 16:34:41 -0600

    update

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Button, Card} from 'react-bootstrap'
+import Rating from '../components/Rating'
+import products from '../products'
 
-const ProductScreen = () => {
+const ProductScreen = ({match}) => {
+  const product = products.find((p) => p._id == match.params.id)
   return (
     <div>
-      Product
+      <Link to='/' className='btn btn-light my-3'>Back to Home</Link>
+      <p>{product.name}</p>
     </div>
   )
 }
