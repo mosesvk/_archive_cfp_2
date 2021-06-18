07e6c2e0015c28cee0bfc0cbda6c6f908b1f0fb3
mosesvk <kaumatule93@gmail.com>
2021-06-18 14:53:42 -0600

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,20 @@
-
-import './App.css';
+import { Container } from 'react-bootstrap'
+import { HashRouter as Router, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <main>
+        <Container>
+          <Route path='/' component={HomeScreen} exact />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 }
 
