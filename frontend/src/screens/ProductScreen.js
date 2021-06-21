b06e19eb6e5310858fe8f3acd70d6cde3ac04d12
mosesvk <kaumatule93@gmail.com>
2021-06-21 15:52:31 -0600

    update

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -13,7 +13,25 @@ const ProductScreen = ({match}) => {
         <Col md={6}>
           <Image src={product.image} alt={product.name}/>
         </Col>
-        <Col md={3}></Col>
+        <Col md={3}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              <Rating value={product.rating} text={`${product.numReviews} reviews`} color={'#f8e825'} />
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              Price: ${product.price}
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              Description: ${product.description}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
         <Col md={3}></Col>
       </Row>
     </div>
