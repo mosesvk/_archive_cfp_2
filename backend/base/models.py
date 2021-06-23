4ce9ab517939abb1f959387dc1883627c843a210
mosesvk <kaumatule93@gmail.com>
2021-06-23 16:56:18 -0600

    update

--- backend/base/models.py
+++ backend/base/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.fields import NullBooleanField
 
 # Create Database 
 # Create your models here.
@@ -7,7 +8,7 @@ from django.contrib.auth.models import User
 class Product(models.Model):
   user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
   name = models.CharField(max_length=200, null=True, blank=True)
-  # image = 
+  image = models.ImageField(null=True, blank=True)
   category = models.CharField(max_length=200, null=True, blank=True)
   description = models.TextField(null=True, blank=True)
   rating = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
