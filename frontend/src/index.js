20875541655ad66bbe123808ecf96c2c8b76ff1d
mosesvk <kaumatule93@gmail.com>
2021-06-24 18:30:15 -0600

    update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Provider } from 'react-redux'
+import store from './store'
+
 import './styles/index.scss';
 import './styles/bootstrap.min.css'
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
