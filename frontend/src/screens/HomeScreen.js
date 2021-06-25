3458d3fd3178fa3e80bc400c54ba6032c61ebb67
mosesvk <kaumatule93@gmail.com>
2021-06-24 19:22:33 -0600

    update

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,31 +1,36 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import Product from '../components/Product'
-import { Row, Col } from 'react-bootstrap'
-import { listProducts } from '../actions/productActions'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Product from "../components/Product";
+import { Row, Col } from "react-bootstrap";
+import { listProducts } from "../actions/productActions";
 
 const HomeScreen = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { error, loading, products } = productList;
 
   useEffect(() => {
-    dispatch(listProducts())
-
-  }, [])
-
-  const products = []
+    dispatch(listProducts());
+  }, [dispatch]);
 
   return (
     <div>
       <h1>This Week's Flavors</h1>
-      <Row>
-        {products.map(product => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product}/>
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
