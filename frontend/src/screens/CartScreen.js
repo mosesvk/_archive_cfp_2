9c6c076429ce47efdfa7f4f8916997eb43c882df
mosesvk <kaumatule93@gmail.com>
2021-06-25 11:25:41 -0600

    update

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -2,7 +2,7 @@ import React, {useEffect} from 'react'
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
-import { Message } from '../components/Message'
+import Message from '../components/Message'
 import { addToCart } from '../actions/cartActions'
 
 const CartScreen = ({match, location, history}) => {
@@ -13,7 +13,6 @@ const CartScreen = ({match, location, history}) => {
 
   const cart = useSelector(state => state.cart)
   const {cartItems} = cart
-  console.log('cartItems: ', cartItems)
 
   useEffect(() => {
     if(productId){
@@ -23,9 +22,36 @@ const CartScreen = ({match, location, history}) => {
 
 
   return (
-    <div>
-      Cart
-    </div>
+    <Row>
+      <Col md={8}>
+        <h1>Shopping Cart</h1>
+        {cartItems.length === 0 ? (
+          <Message>
+            Your cart is empty <Link to='/'>Go back</Link>
+          </Message>
+        ) : (
+          <ListGroup flush='variant'>
+            {cartItems.map(item => (
+              <ListGroup.Item key={item.product}>
+                <Row>
+                  <Col md={2}>
+                    <Image src={item.image} alt={item.name} fluid rounded/>
+                  </Col>
+                  <Col md={3}>
+                    <Link to={`/product/${item.product}`}>{item.name}</Link>
+                  </Col>
+                  <Col md={2}>${item.price}</Col>
+                </Row>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        )}
+      </Col>
+
+      <Col md={8}>
+
+      </Col>
+    </Row>
   )
 }
 
