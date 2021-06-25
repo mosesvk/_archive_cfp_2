fb5dc1bdda46aa1d17ee57cf8fd1f7d76895f032
mosesvk <kaumatule93@gmail.com>
2021-06-24 19:38:05 -0600

    update ; ;

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Product from "../components/Product";
 import Loader from "../components/Loader";
+import Message from "../components/Message";
 import { Row, Col } from "react-bootstrap";
 import { listProducts } from "../actions/productActions";
 
@@ -20,7 +21,7 @@ const HomeScreen = () => {
       {loading ? (
         <Loader />
       ) : error ? (
-        <h3>{error}</h3>
+        <Message variant='danger'>{error}</Message>
       ) : (
         <Row>
           {products.map((product) => (
