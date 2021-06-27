f6499cd208d51f7e10a016bc9d204e01ab195302
mosesvk <kaumatule93@gmail.com>
2021-06-27 08:27:58 -0600

    update

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -60,7 +60,7 @@ SIMPLE_JWT = {
     'UPDATE_LAST_LOGIN': False,
 
     'ALGORITHM': 'HS256',
-    'SIGNING_KEY': settings.SECRET_KEY,
+    # 'SIGNING_KEY': settings.SECRET_KEY,
     'VERIFYING_KEY': None,
     'AUDIENCE': None,
     'ISSUER': None,
