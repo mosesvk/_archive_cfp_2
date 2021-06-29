5421ed40e81b85245e3158a2f9ec130f3a4826fa
mosesvk <kaumatule93@gmail.com>
2021-06-29 16:09:42 -0600

    update

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -7,12 +7,49 @@ import Message from '../components/Message'
 import {getUserDetails} from '../components/FormContainer'
 
 const ProfileScreen = ({history}) => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+  const [message, setMessage] = useState('')
+
+  const dispatch = useDispatch()
+
+  const userDetails = useSelector(state => state.userDetails)
+  const {error, loading, user} = userDetails
+
+  const userLogin = useSelector(state => state.userLogin)
+  const { userInfo } = userLogin
+
+  useEffect(() => {
+    if (!userInfo){
+      history.push('/login')
+    } else {
+      if (!user || !user.name) {
+        dispatch(getUserDetails('profile'))
+      } else {
+        setName(user.name)
+        setEmail(user.email)
+      }
+    }
+  }, [dispatch, history, userInfo, user])
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+
+    if (password != confirmPassword){
+      setMessage('Passwords do not match')
+    } else {
+      console.log('udpating...')
+    }
+  }
+
   return (
     <Row>
       <Col md={3}>
         <h2>User Profile</h2>
       </Col>
-      
+
       <Col md={9}>
         <h2>My Orders</h2>
       </Col>
