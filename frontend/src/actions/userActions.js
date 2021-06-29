41f7c90dfdb6de322238b4547432a6041108c7e9
mosesvk <kaumatule93@gmail.com>
2021-06-29 09:49:59 -0600

    update

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -34,7 +34,7 @@ export const login = (email, password) => async (dispatch) => {
   }catch(error){
     dispatch({
       type: USER_LOGIN_FAIL, 
-      payload: error.response && error.reponse.data.detail
+      payload: error.response && error.response.data.detail
       ? error.response.data.detail
       : error.message,
     })
