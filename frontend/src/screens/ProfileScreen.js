d6e2d14191afca6cff2b7df86b500bcae351387c
mosesvk <kaumatule93@gmail.com>
2021-06-29 18:14:31 -0600

    update

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -4,7 +4,7 @@ import {Form, Button, Row, Col} from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
-import {getUserDetails} from '../components/FormContainer'
+import {getUserDetails} from '../actions/userActions'
 
 const ProfileScreen = ({history}) => {
   const [name, setName] = useState('')
@@ -48,6 +48,66 @@ const ProfileScreen = ({history}) => {
     <Row>
       <Col md={3}>
         <h2>User Profile</h2>
+
+        {message && <Message variant='danger'>{message}</Message>}
+        {error && <Message variant='danger'>{error}</Message>}
+        {loading && <Loader/>}
+
+        <Form onSubmit={submitHandler}>
+
+          <Form.Group controlId='name'>
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type='name'
+                placeholder='Enter Name'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              >
+              </Form.Control>
+          </Form.Group>
+
+          <Form.Group controlId='email'>
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control
+              type='email'
+              placeholder='Enter Email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            >
+            </Form.Control>
+          </Form.Group>
+
+          <Form.Group controlId='password'>
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type='password'
+              placeholder='Enter Password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            >
+            </Form.Control>
+          </Form.Group>
+
+          <Form.Group controlId='passwordConfirm'>
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control
+              type='password'
+              placeholder='Confirm Password'
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            >
+            </Form.Control>
+          </Form.Group>
+
+          <Button type='submit' variant='primary'>
+            Update
+          </Button>
+
+          <Row className='py-3'>
+
+        </Row>
+
+      </Form>
       </Col>
 
       <Col md={9}>
