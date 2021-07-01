e408683f4de7894cc1938df4e7a4e1712456ce17
mosesvk <kaumatule93@gmail.com>
2021-07-01 09:03:59 -0600

    update

---
+++ frontend/src/screens/UserEditScreen.js
@@ -0,0 +1,88 @@
+import React, {useState, useEffect} from 'react'
+import {Link} from 'react-router-dom'
+import {Form, Button} from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+import FormContainer from '../components/FormContainer'
+
+import { getUserDetails } from '../actions/userActions'
+
+const EditUserScreen = ({match, history}) => {
+
+  const userId = match.params.id
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [isAdmin, setIsAdmin] = useState('')
+
+  const dispatch = useDispatch()
+
+  const userDetails = useSelector(state => state.userDetails)
+  const {error, loading, user} = userDetails
+  // This is grabbed from the userReducers file 
+
+  useEffect(() => {
+  
+  }, [])
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+
+  }
+
+  return (
+    <div>
+      <Link to='/admin/userlist'>Go Back</Link>
+
+      <FormContainer>
+        <h1>Edit User </h1>
+        {loading ? <Loader/> : error ? <Message variant='danger'>{error}</Message> : (
+          <Form onSubmit={submitHandler}>
+
+            <Form.Group controlId='name'>
+                <Form.Label>Name</Form.Label>
+                <Form.Control
+                  type='name'
+                  placeholder='Enter Name'
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                >
+                </Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId='email'>
+              <Form.Label>Email Address</Form.Label>
+              <Form.Control
+                type='email'
+                placeholder='Enter Email'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              >
+              </Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId='isAdmin'>
+              <Form.Check
+                type='checkbox'
+                label='Is Admin'
+                checked={isAdmin}
+                onChange={(e) => setIsAdmin(e.target.checked)}
+              >
+              </Form.Check>
+            </Form.Group>
+
+            <Button type='submit' variant='primary'>
+              Register
+            </Button> 
+
+          </Form>
+        )}
+      </FormContainer>
+  
+    </div>
+
+  )
+}
+
+export default EditUserScreen
