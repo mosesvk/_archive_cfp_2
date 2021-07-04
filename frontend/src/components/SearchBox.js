01a24cfbf2be151b0556d6b3abe4ad161e2f661a
mosesvk <kaumatule93@gmail.com>
2021-07-03 20:54:13 -0600

    update

--- frontend/src/components/SearchBox.js
+++ frontend/src/components/SearchBox.js
@@ -2,31 +2,37 @@ import React, { useState } from 'react'
 import { Button, Form } from 'react-bootstrap'
 import { useHistory } from 'react-router-dom'
 
-const SearchBox = () => {
-  const [keyword, setKeyword] = useState('')
+function SearchBox() {
+    const [keyword, setKeyword] = useState('')
 
-  const submitHandler = (e) => {
-    e.preventDefault()
-  }
+    let history = useHistory()
 
-  return (
-    <Form onSubmit={submitHandler} inline>
-      <Form.Control
-        type='text'
-        name='q'
-        onChange={(e) => setKeyword(e.target.value)}
-        className='mr-sm-2 ml-sm-5'
-      ></Form.Control>
+    const submitHandler = (e) => {
+        e.preventDefault()
+        if (keyword) {
+            history.push(`/?keyword=${keyword}&page=1`)
+        } else {
+            history.push(history.push(history.location.pathname))
+        }
+    }
+    return (
+        <Form onSubmit={submitHandler} >
+            <Form.Control
+                type='text'
+                name='q'
+                onChange={(e) => setKeyword(e.target.value)}
+                className='mr-sm-1 ml-sm-5'
+            ></Form.Control>
 
-      <Button
-        type='submit'
-        variant='outline-success'
-        className='p-2'
-      >
-        Submit
-      </Button>
-    </Form>
-  )
+            <Button
+                type='submit'
+                variant='outline-success'
+                className='p-2'
+            >
+                Submit
+            </Button>
+        </Form>
+    )
 }
 
 export default SearchBox
