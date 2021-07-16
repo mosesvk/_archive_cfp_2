3f90255b0c034726b1eefa7c98d0ec8d9f331302
mosesvk <kaumatule93@gmail.com>
2021-07-15 23:38:24 -0600

    update

--- backend/frontend/src/components/ProductCarousel.js
+++ backend/frontend/src/components/ProductCarousel.js
@@ -36,7 +36,7 @@ function ProductCarousel() {
                             <p>Take a look at our selection of sweet fried pies</p>
                         </Carousel.Caption>
                     </Carousel.Item>
-                    {/* <Carousel.Item>
+                    <Carousel.Item>
                         <img
                         className="d-block w-100"
                         src="holder.js/800x400?text=Third slide&bg=20232a"
@@ -44,10 +44,10 @@ function ProductCarousel() {
                         />
 
                         <Carousel.Caption>
-                        <h3>Third slide label</h3>
+
                         <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
                         </Carousel.Caption>
-                    </Carousel.Item> */}
+                    </Carousel.Item>
                 </Carousel>
             )
 
