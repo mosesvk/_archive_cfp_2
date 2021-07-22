366a66e7a4911d8978d32cda1a8ccf7dd2b84f96
mosesvk <kaumatule93@gmail.com>
2021-07-21 20:11:43 -0600

    update

--- backend/frontend/src/components/ProductCarousel.js
+++ backend/frontend/src/components/ProductCarousel.js
@@ -23,11 +23,6 @@ function ProductCarousel() {
                 <Carousel className='bg-dark carousel-container' fade>
 
                     <Carousel.Item className='h-30 d-flex justify-content-center carousel-sweet'>
-                        <Image
-                        className="d-block w-100"
-                        src="https://scontent-lax3-2.xx.fbcdn.net/v/t1.6435-9/117607715_148405643567652_8532818459016486806_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=e3f864&_nc_ohc=EkTxLPQv4a0AX_7QGYL&_nc_ht=scontent-lax3-2.xx&oh=90dabf0d7c0f02ab8e563492ffdf160b&oe=60FB79EA"
-                        alt="Second slide"
-                        />
 
                         <Carousel.Caption className='d-flex align-items-center carousel-caption'>
                             <Image className='w-25' src={'https://scontent-lax3-2.xx.fbcdn.net/v/t1.6435-9/117392708_148406646900885_2986494580076297624_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=DNswBhgsJjMAX9NAFUo&_nc_ht=scontent-lax3-2.xx&oh=cbc219369c29dbdd832520d4c8c3828f&oe=60FB686E'} style={{borderRadius:'50%'}}/>
