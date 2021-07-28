0638cd42568ac03646ca524cd7ea4341ee569a99
mosesvk <kaumatule93@gmail.com>
2021-07-27 21:15:21 -0600

    update

---
+++ backend/base/views/display_views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+from base.models import Product, Review
+from base.serializers import ProductSerializer
+
+from rest_framework import status
