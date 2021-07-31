452e6d19627c50a4cef1f587414869ff2060ebe1
mosesvk <kaumatule93@gmail.com>
2021-07-30 22:43:44 -0600

    update

--- backend/base/urls/display_urls.py
+++ backend/base/urls/display_urls.py
@@ -3,13 +3,13 @@ from base.views import display_views as views
 
 urlpatterns = [
 
-    path('', views.getDisplay, name="Displays"),
+    # path('', views.getDisplay, name="Displays"),
 
     path('create/', views.createDisplay, name="display-create"),
-    path('upload/', views.uploadImage, name="image-upload"),
+    # path('upload/', views.uploadImage, name="image-upload"),
 
-    path('<str:pk>/', views.getProduct, name="product"),
+    # path('<str:pk>/', views.getProduct, name="product"),
 
-    path('update/<str:pk>/', views.updateDisplay, name="product-display"),
-    path('delete/<str:pk>/', views.deleteDisplay, name="product-display"),
+    # path('update/<str:pk>/', views.updateDisplay, name="product-display"),
+    # path('delete/<str:pk>/', views.deleteDisplay, name="product-display"),
 ]
