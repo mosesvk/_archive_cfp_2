852fb3de14ae6fc04f70ec73eb9429ff256a0a60
mosesvk <kaumatule93@gmail.com>
2021-08-02 21:05:20 -0600

    update

--- backend/base/models.py
+++ backend/base/models.py
@@ -83,3 +83,6 @@ class Display(models.Model):
     image = models.ImageField(null=True, blank=True, default='/sample.jpg')
     description = models.TextField(null=True, blank=True)
     _id = models.AutoField(primary_key=True, editable=False)
+    
+    def __str__(self):
+        return str(self.name)
