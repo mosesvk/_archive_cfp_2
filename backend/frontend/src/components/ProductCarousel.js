52c9990a66b0832e87102f6029921b7022a62e28
mosesvk <kaumatule93@gmail.com>
2021-08-06 22:49:10 -0600

    update ;

--- backend/frontend/src/components/ProductCarousel.js
+++ backend/frontend/src/components/ProductCarousel.js
@@ -23,6 +23,7 @@ function ProductCarousel() {
             <Carousel.Item className='h-30 d-flex justify-content-center carousel-sweet'>
                 <Carousel.Caption className='d-flex align-items-center carousel-caption' fluid>
                     <Image className='logo-img' src='../images/cfp-cover-1.png' alt='logo-img' fluid />
+                    <h1>Have a Sweet Tooth?</h1>
                     <Button className='btn-sweet' variant="primary">Check it Out</Button>
                 </Carousel.Caption>
             </Carousel.Item>
--- backend/frontend/src/screens/CartScreen.js
+++ backend/frontend/src/screens/CartScreen.js
@@ -48,7 +48,7 @@ const CartScreen = ({match, location, history}) => {
                   <Col md={3}>
                     <Link to={`/product/${item.product}`}>{item.name}</Link>
                   </Col>
-                  <Col md={2}>${item.price}</Col>
+                  <Col md={2}>${item.price} each</Col>
                   <Col md={3}>
                     <Form.Control
                       className='form-select'
