7df7ddfb494e716f92de57c54cc53c9d18267937
mosesvk <kaumatule93@gmail.com>
2021-08-07 23:39:04 -0600

    update

--- backend/frontend/src/components/Sweet.js
+++ backend/frontend/src/components/Sweet.js
@@ -1,11 +1,18 @@
 import React from 'react'
-import { Container, Image } from 'react-bootstrap'
+import { Container, Image, Row, Col } from 'react-bootstrap'
 import logoImg from '../images/cfp-cover-1.png'
 
 const Sweet = () => {
   return (
     <Container className='sweet-container'>
-      <Image src={logoImg} className='logo-img'/>
+      <Row>
+        <Col md={4}>
+          <Image src={logoImg} className='logo-img'/>
+        </Col>
+        <Col md={3}>
+          <h1>Sweet Flavors</h1>
+        </Col>
+      </Row>
     </Container>
   )
 }
