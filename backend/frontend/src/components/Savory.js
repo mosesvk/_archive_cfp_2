6227fa0f5f81b7e2ff652ada55819cc9d10016f3
mosesvk <kaumatule93@gmail.com>
2021-08-09 11:41:49 -0600

    update

---
+++ backend/frontend/src/components/Savory.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Container, Image, Row, Col } from 'react-bootstrap'
+import logoImg from '../images/cfp-cover-1.png'
+
+const Savory = () => {
+  return (
+    <Container className='sweet-container'>
+      <Row>
+        <Col md={4}>
+          <Image src={logoImg} className='logo-img'/>
+        </Col>
+        <Col md={3}>
+          <h1>Sweet Flavors</h1>
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default Savory
