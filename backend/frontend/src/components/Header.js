7b85e89d26e1b8f441ef1f9c8480e96a0831d67f
mosesvk <kaumatule93@gmail.com>
2021-08-18 23:17:22 -0600

    update

--- backend/frontend/src/components/Header.js
+++ backend/frontend/src/components/Header.js
@@ -44,23 +44,6 @@ const Header = ({history}) => {
                     <NavDropdown.Item>Profile</NavDropdown.Item>
                   </LinkContainer>
 
-                  {/* {userInfo && userInfo.isAdmin && (
-                    <NavDropdown title='Admin' variant='light' style={{color: 'black'}}>
-                      <LinkContainer to='/admin/userlist'>
-                        <NavDropdown.Item>Users</NavDropdown.Item>
-                      </LinkContainer>
-
-                      <LinkContainer to='/admin/productlist'>
-                        <NavDropdown.Item>Products</NavDropdown.Item>
-                      </LinkContainer>
-
-                      <LinkContainer to='/admin/orderlist'>
-                        <NavDropdown.Item>Orders</NavDropdown.Item>
-                      </LinkContainer>
-
-                    </NavDropdown>
-                  )} */}
-
                   <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
 
                 </NavDropdown>
@@ -87,3 +70,22 @@ const Header = ({history}) => {
 }
 
 export default Header
+
+
+
+                  /* {userInfo && userInfo.isAdmin && (
+                    <NavDropdown title='Admin' variant='light' style={{color: 'black'}}>
+                      <LinkContainer to='/admin/userlist'>
+                        <NavDropdown.Item>Users</NavDropdown.Item>
+                      </LinkContainer>
+
+                      <LinkContainer to='/admin/productlist'>
+                        <NavDropdown.Item>Products</NavDropdown.Item>
+                      </LinkContainer>
+
+                      <LinkContainer to='/admin/orderlist'>
+                        <NavDropdown.Item>Orders</NavDropdown.Item>
+                      </LinkContainer>
+
+                    </NavDropdown>
+                  )} */
