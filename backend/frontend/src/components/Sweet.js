aa550df5d72eca3ddd1d196cea013c654dddca2e
mosesvk <kaumatule93@gmail.com>
2021-10-04 15:08:05 -0600

    update

--- backend/frontend/src/components/Sweet.js
+++ backend/frontend/src/components/Sweet.js
@@ -42,7 +42,7 @@ const Sweet = ({ history }) => {
                     :
                     <div>
                         <Row>
-                            {products.filter(product => product.category = 'sweet').map(product => (
+                            {products.map(product => product.category = 'sweet').map(product => (
                                 <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                                     <Product product={product} />
                                 </Col>
