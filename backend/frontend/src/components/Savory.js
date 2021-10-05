dc60ce2f4f93110432c8d1605e42cfdba8c481bb
mosesvk <kaumatule93@gmail.com>
2021-10-05 01:09:57 -0600

    udpate

--- backend/frontend/src/components/Savory.js
+++ backend/frontend/src/components/Savory.js
@@ -1,8 +1,28 @@
-import React from 'react'
-import { Container, Image, Row, Col } from 'react-bootstrap'
+import React, { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import Product from '../components/Product'
 import logoImg from '../images/cfp-cover-1.png'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+import Paginate from '../components/Paginate'
+import { Container, Image, Row, Col } from 'react-bootstrap'
+import { listProducts } from '../actions/productActions'
 
-const Savory = () => {
+const Savory = ({history}) => {
+
+  const dispatch = useDispatch()
+
+  const productList = useSelector(state => state.productList)
+  const { error, loading, products, page, pages } = productList
+
+  let keyword = history.location.search
+
+  useEffect(() => {
+      dispatch(listProducts(keyword))
+  }, [dispatch, keyword])
+
+  let savoryProducts = products.filter(elem => elem.category == 'savory')
+
   return (
     <Container className='savory-container'>
       <Row>
@@ -14,6 +34,20 @@ const Savory = () => {
           <p>Try our Savory Fried Pies</p>
         </Col>
       </Row>
+      {loading ? <Loader />
+                : error ? <Message variant='danger'>{error}</Message>
+                    :
+                    <div>
+                        <Row>
+                            {savoryProducts.map(product => (
+                              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                                    <Product product={product} />
+                                </Col>
+                            ))}
+                        </Row>
+                        <Paginate page={page} pages={pages} keyword={keyword} />
+                    </div>
+      }
     </Container>
   )
 }
