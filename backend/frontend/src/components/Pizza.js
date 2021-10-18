0c48d295fe3d9311624d47a4da5b6099a5d63705
mosesvk <kaumatule93@gmail.com>
2021-10-18 14:29:42 -0600

    update

--- backend/frontend/src/components/Pizza.js
+++ backend/frontend/src/components/Pizza.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Container, Image, Row, Col } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 import logoImg from '../images/cfp-cover-1.png'
 
 const Pizza = () => {
   return (
     <Container className='pizza-container'>
       <Row>
+        <Link to='/' className='btn btn-light my-3'>Go Back</Link>
         <Col md={4}>
           <Image src={logoImg} className='logo-img'/>
         </Col>
