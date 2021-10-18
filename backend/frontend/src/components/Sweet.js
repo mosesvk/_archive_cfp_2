27088fa27d00c990b4c92f53be4624e9975e6fc3
mosesvk <kaumatule93@gmail.com>
2021-10-17 22:16:11 -0600

    update

--- backend/frontend/src/components/Sweet.js
+++ backend/frontend/src/components/Sweet.js
@@ -38,7 +38,7 @@ const Sweet = ({ history }) => {
                 : error ? <Message variant='danger'>{error}</Message>
                     :
                     <div>
-                        <Row>
+                        <Row className='home-screen-row'>
                             {sweetProducts.map(product => (
                               <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                                     <Product product={product} />
