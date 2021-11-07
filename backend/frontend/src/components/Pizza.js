df0295b2fc5c2c3bb06e3d1a9fc0ab4206300ac8
mosesvk <kaumatule93@gmail.com>
2021-11-06 20:37:26 -0600

    update

--- backend/frontend/src/components/Pizza.js
+++ backend/frontend/src/components/Pizza.js
@@ -28,9 +28,6 @@ const Pizza = ({history}) => {
     <Container className='pizza-container'>
       <Row>
         <Link to='/' className='btn btn-light my-3'>Go Back</Link>
-        <Col md={4}>
-          <Image src={logoImg} className='logo-img'/>
-        </Col>
         <Col md={3}>
           <h1>Pizza Puffs!</h1>
           <p>Try our new Fried Pizza Puffs</p>
