767fb1c403e4fcc3c395db96ddb29d113118aa0a
mosesvk <kaumatule93@gmail.com>
2021-11-07 08:40:47 -0700

    update

--- backend/frontend/src/components/Sweet.js
+++ backend/frontend/src/components/Sweet.js
@@ -1,33 +1,34 @@
-import React, { useState, useEffect } from 'react'
-import { Container, Image, Row, Col } from 'react-bootstrap'
-import { useDispatch, useSelector } from 'react-redux'
-import Product from '../components/Product'
-import logoImg from '../images/cfp-cover-1.png'
-import { Link } from 'react-router-dom'
-import Loader from '../components/Loader'
-import Message from '../components/Message'
-import Paginate from '../components/Paginate'
-import { listProducts } from '../actions/productActions'
+import React, { useState, useEffect } from "react";
+import { Container, Image, Row, Col } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import Product from "../components/Product";
+import logoImg from "../images/cfp-cover-1.png";
+import { Link } from "react-router-dom";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+import Paginate from "../components/Paginate";
+import { listProducts } from "../actions/productActions";
 
 const Sweet = ({ history }) => {
+  const dispatch = useDispatch();
 
-  const dispatch = useDispatch()
+  const productList = useSelector((state) => state.productList);
+  const { error, loading, products, page, pages } = productList;
 
-  const productList = useSelector(state => state.productList)
-  const { error, loading, products, page, pages } = productList
-
-  let keyword = history.location.search
+  let keyword = history.location.search;
 
   useEffect(() => {
-      dispatch(listProducts(keyword))
-  }, [dispatch, keyword])
+    dispatch(listProducts(keyword));
+  }, [dispatch, keyword]);
 
-  let sweetProducts = products.filter(elem => elem.category == 'sweet')
+  let sweetProducts = products.filter((elem) => elem.category == "sweet");
 
   return (
-    <Container className='sweet-container'>
+    <Container className="sweet-container">
       <Row>
-        <Link to='/' className='btn btn-light my-3'>Go Back</Link>
+        <Link to="/" className="btn btn-light my-3">
+          Go Back
+        </Link>
         {/* <Col md={4}>
           <Image src={logoImg} className='logo-img'/>
         </Col> */}
@@ -36,22 +37,24 @@ const Sweet = ({ history }) => {
         </Col>
       </Row>
 
-      {loading ? <Loader />
-                : error ? <Message variant='danger'>{error}</Message>
-                    :
-                    <div>
-                        <Row className='justify-center'>
-                            {sweetProducts.map(product => (
-                              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                                    <Product product={product} />
-                                </Col>
-                            ))}
-                        </Row>
-                        <Paginate page={page} pages={pages} keyword={keyword} />
-                    </div>
-      }
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <div>
+          <Row className="justify-center">
+            {sweetProducts.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+          <Paginate page={page} pages={pages} keyword={keyword} />
+        </div>
+      )}
     </Container>
-  )
-}
+  );
+};
 
-export default Sweet
+export default Sweet;
